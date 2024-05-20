79a7588c7b40f60c5a69e4cc124c1db94b8517f0
afsar-uddin <afsarwebdev@gmail.com>
2024-05-20 07:54:51 +0600

    convert to react only

--- src/app/about/page.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import SiteHeader from '../components/SiteHeader'
-
-export default function page() {
-  return (
-    <SiteHeader />
-  )
-}
--- src/app/blog/[postSlug]/page.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Post() {
-  return (
-    <div>This is just a sample post</div>
-  )
-}
