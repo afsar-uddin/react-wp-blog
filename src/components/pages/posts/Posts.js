0c6f6dee3f6798c1b992de2cab9488e82537cb6b
afsar-uddin <afsarwebdev@gmail.com>
2024-05-28 08:30:39 +0600

    post pagination

--- src/components/pages/posts/Posts.js
+++ src/components/pages/posts/Posts.js
@@ -1,17 +1,24 @@
+import { current } from '@reduxjs/toolkit';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 
 function Posts() {
-    let url = `${process.env.REACT_APP_API_ROOT}/posts`;
-    const [posts, setPost] = useState([]);
+    // let url = `${process.env.REACT_APP_API_ROOT}/posts`;
+    const [totalPages, setTotalPages] = useState(1);
+    const [currentPage, setCurrentPage] = useState(1);
 
+    const [posts, setPost] = useState([]);
+    
     useEffect(() => {
+        let url = `${process.env.REACT_APP_API_ROOT}/posts?per_page=10&page=${currentPage}`;
         axios.get(url).then((res) => {
+            const {data, headers} = res;
+            setTotalPages(Number(headers['x-wp-totalpages']));
             setPost(res.data)
         });
 
-    }, [])
+    }, [currentPage])
     
   return (
     <>
@@ -28,6 +35,18 @@ function Posts() {
                 )
             }) : 'Loading'
         }
+
+        {/* pagination */}
+        <div className='w-4/5 py-10 m-auto flex justify-between align-middle flex-wrap gap-10'>
+            <button className='btn-primary' 
+            disabled = {currentPage === 1}
+            onClick={() => setCurrentPage(currentPage - 1)}>Previous</button>
+            {/* pagination number */}
+            <span>{currentPage} of {totalPages}</span>
+            <button className='btn-primary' 
+            disabled = {currentPage === totalPages}
+            onClick={() => setCurrentPage(currentPage + 1)}>Next</button>
+        </div>
     </>
   )
 }
